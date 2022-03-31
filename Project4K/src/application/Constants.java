58559ada748f8caf36da989d9687b49ad9c96217
eckoo <ktking52@gmail.com>
2022-03-31 18:33:41 -0400

    updated with constants file

---
+++ Project4K/src/application/Constants.java
@@ -0,0 +1,22 @@
+package application;
+
+public class Constants {
+	public static final double tallSizePriceIncrease = 0.40;
+	public static final double grandeSizePriceIncrease = 0.40;
+	public static final double ventiSizePriceIncrease = 0.40;
+
+	public static final double baseCoffeePrice = 1.69; //this is for a short size cup
+	public static final double addInPrice = 0.30;
+	public static final double baseYeastDonutPrice = 1.59;
+	public static final double baseCakeDonutPrice = 1.79;
+	public static final double baseDonutHolePrice = 0.39;
+	
+	public static final String SHORT = "short";
+	public static final String TALL = "tall";
+	public static final String GRANDE = "grande";
+	public static final String VENTI = "venti";
+	
+	public static final String CAKE = "cake donut";
+	public static final String YEAST = "yeast donut";
+	public static final String HOLE = "donut hole";
+}
